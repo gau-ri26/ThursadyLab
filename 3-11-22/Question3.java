6d7b84ebbb7937a0f98337e4be43b90635cbd717
Gauri Balghare <113991032+gau-ri26@users.noreply.github.com>
2022-11-03 21:28:31 +0530

    Create Question3.java

---
+++ 3-11-22/Question3.java
@@ -0,0 +1,83 @@
+/* Write a JAVA program which will generate the threads: - To display 10 terms of Fibonacci series. - To display 1 to 10 in reverse order. */
+
+
+package Nov_03_11;
+
+import java.io.*;
+
+public class FibnoiccThread extends Thread
+{
+	public void run()  //Calling run method in another class
+    {
+         try
+         {
+              int a=0, b=1, c=0;
+              BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+              System.out.print("Enter the Limit for fabonacci: ");
+
+              int n = Integer.parseInt(br.readLine());
+              
+              System.out.println("Fibonacci series:");
+              while (n>0)
+              {
+                   System.out.print(c+" ");
+                   a=b;
+                   b=c;
+                   c=a+b;
+                   n=n-1;
+              }
+         }
+         catch (Exception ex)
+         {
+              ex.printStackTrace();
+         }
+    }
+}
+
+package Nov_03_11;
+
+public class ReverseThread extends Thread
+{
+	public void run()   //Calling run method in another class
+    {
+         try
+         {
+              
+              System.out.println("\n Reverse is: ");
+              
+              for (int i=10; i >= 1 ;i-- )
+              {
+                   System.out.print(i+"  ");
+              }
+              
+         }
+         catch (Exception ex)
+         {
+              ex.printStackTrace();
+         }
+    }
+}
+
+package Nov_03_11;
+
+public class MainThreadQ2 
+{  
+  //main method
+	public static void main(String[] args)
+    {
+         try
+         {
+              FibnoiccThread fib = new FibnoiccThread();
+              fib.start();            // calling the run method
+              fib.sleep(5000);         //sleeping thread for 5sec
+              ReverseThread rev = new ReverseThread();
+              rev.start();          // calling the run method
+         }
+         catch (Exception ex) 
+         {
+              ex.printStackTrace();
+         }
+    }
+}
+
